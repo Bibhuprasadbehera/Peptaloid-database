b637956f4293ece830bf4ff4683f86a0b6746e4b
Bibhuprasadbehera <53312940+Bibhuprasadbehera@users.noreply.github.com>
2024-05-30 12:16:08 +0530

    updated navabr and carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,43 +1,57 @@
 // Carousel.js
-
 import React, { useState, useEffect } from 'react';
-import './Carousel.css'; // Import carousel styles
-import image1 from '../images/molecule1.jpg'; // Sample image
-import image2 from '../images/molecule2.jpg'; // Sample image
-import image3 from '../images/molecule3.jpg'; // Sample image
+import './Carousel.css';
+
+import image1 from '../images/molecule1.jpg';
+import image2 from '../images/molecule2.jpg';
+import image3 from '../images/molecule3.jpg';
 
 const Carousel = () => {
   const images = [image1, image2, image3];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+      setIsTransitioning(true);
+      setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+        setIsTransitioning(false);
+      }, 500); // Adjust the delay (in milliseconds) for the transition duration
     }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
   const goToPreviousImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+      setIsTransitioning(false);
+    }, 500); // Adjust the delay (in milliseconds) for the transition duration
   };
 
   const goToNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+      setIsTransitioning(false);
+    }, 500); // Adjust the delay (in milliseconds) for the transition duration
   };
 
   return (
     <div className="carousel">
-      {images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt={`Molecule ${index + 1}`}
-          className={index === currentImageIndex ? 'carousel-image active' : 'carousel-image'}
-        />
-      ))}
-      <button className="prev-button" onClick={goToPreviousImage}>&#10094;</button>
-      <button className="next-button" onClick={goToNextImage}>&#10095;</button>
+      <img
+        src={images[currentImageIndex]}
+        alt={`Molecule ${currentImageIndex + 1}`}
+        className={`carousel-image ${isTransitioning ? 'transitioning' : ''}`}
+      />
+      <button className="prev-button" onClick={goToPreviousImage}>
+        &#10094;
+      </button>
+      <button className="next-button" onClick={goToNextImage}>
+        &#10095;
+      </button>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,19 +6,23 @@ import logo from '../images/logo2.png'; // Adjust the path according to your dir
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="logo-link">
-        <img src={logo} alt="Peptaloid" className="logo" />
-      </Link>
-      <ul>
-        <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
-        <li><NavLink to="/help" activeClassName="active">Help</NavLink></li>
-        <li><NavLink to="/learn" activeClassName="active">Learn</NavLink></li>
-        <li><NavLink to="/search" activeClassName="active">Search</NavLink></li>
-        <li><NavLink to="/download" activeClassName="active">Download</NavLink></li>
-        <li><NavLink to="/faq" activeClassName="active">FAQ</NavLink></li>
-        <li><NavLink to="/login" activeClassName="active">Login</NavLink></li>
-        <li><NavLink to="/tools" activeClassName="active">Tools</NavLink></li>
-      </ul>
+      <div className="logo-container">
+        <Link to="/" className="logo-link">
+          <img src={logo} alt="Peptaloid" className="logo" />
+        </Link>
+      </div>
+      <div className="nav-links-container">
+        <ul className="nav-links">
+          <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+          <li><NavLink to="/help" activeClassName="active">Help</NavLink></li>
+          <li><NavLink to="/learn" activeClassName="active">Learn</NavLink></li>
+          <li><NavLink to="/search" activeClassName="active">Search</NavLink></li>
+          <li><NavLink to="/download" activeClassName="active">Download</NavLink></li>
+          <li><NavLink to="/faq" activeClassName="active">FAQ</NavLink></li>
+          <li><NavLink to="/tools" activeClassName="active">Tools</NavLink></li>
+        </ul>
+        <NavLink to="/login" activeClassName="active" className="login-button">Login</NavLink>
+      </div>
     </nav>
   );
 };
