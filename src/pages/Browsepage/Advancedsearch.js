4a71408b4671fb130d7f835f1bbf5eb3d98b7267
Bibhuprasadbehera <53312940+Bibhuprasadbehera@users.noreply.github.com>
2024-06-19 17:24:23 +0530

    your

--- src/pages/Browsepage/Advancedsearch.js
+++ src/pages/Browsepage/Advancedsearch.js
@@ -37,7 +37,7 @@ const AdvancedSearch = () => {
             <div className="container-search">
                 <h2 className="heading">Advanced Search</h2>
                 <p>Use the advanced search feature to refine your search results. You can add multiple search criteria and specify the logical operator between them.</p>
-               
+
                 <form onSubmit={handleSubmit}>
                     <table className="criteria-table">
                         <thead>
@@ -60,11 +60,58 @@ const AdvancedSearch = () => {
                                             value={criterion.field}
                                             onChange={(e) => handleCriteriaChange(index, 'field', e.target.value)}
                                         >
-                                            <option value="Peptide Name">Peptide Name</option>
-                                            <option value="Peptide Sequence">Peptide Sequence</option>
-                                            <option value="Length">Length</option>
-                                            <option value="Linear/Cyclic">Linear/Cyclic</option>
+                                            <optgroup label="Identifier">
+                                                <option value="InChIKey">InChIKey</option>
+                                                <option value="Compound_InChI">Compound InChI</option>
+                                                <option value="smiles">SMILES</option>
+                                                <option value="IUPAC_Name">IUPAC Name</option>
+                                                <option value="Compound_Name">Compound Name</option>
+                                            </optgroup>
+                                            <optgroup label="Origin">
+                                                <option value="origin_type">Origin Type</option>
+                                                <option value="genus">Genus</option>
+                                                <option value="origin_species">Origin Species</option>
+                                            </optgroup>
+                                            <optgroup label="Physical Properties">
+                                                <option value="carbon_count">Carbon Count</option>
+                                                <option value="Exact_MW">Exact MW</option>
+                                                <option value="Num_Atoms">Number of Atoms</option>
+                                                <option value="Num_Heavy_Atoms">Number of Heavy Atoms</option>
+                                                <option value="Num_Hetero_Atoms">Number of Hetero Atoms</option>
+                                                <option value="Num_StereoCenters">Number of StereoCenters</option>
+                                            </optgroup>
+                                            <optgroup label="Chemical Properties">
+                                                <option value="SlogP">SlogP</option>
+                                                <option value="TPSA">TPSA</option>
+                                                <option value="CX_LogP">CX LogP</option>
+                                                <option value="CX_LogD">CX LogD</option>
+                                                <option value="Heavy_Atoms">Heavy Atoms</option>
+                                                <option value="Lipinski">Lipinski</option>
+                                                <option value="total_charge">Total Charge</option>
+                                                <option value="qed_score">QED Score</option>
+                                            </optgroup>
+                                            <optgroup label="Functional Groups">
+                                                <option value="Alcohol">Alcohol</option>
+                                                <option value="Aldehyde">Aldehyde</option>
+                                                <option value="Ketone">Ketone</option>
+                                                <option value="Carboxylic Acid">Carboxylic Acid</option>
+                                                <option value="Amine">Amine</option>
+                                                <option value="Thiol">Thiol</option>
+                                            </optgroup>
+                                            <optgroup label="Bond Types">
+                                                <option value="Num_Rotatable_Bonds">Number of Rotatable Bonds</option>
+                                                <option value="Num_HBD">Number of HBD</option>
+                                                <option value="Num_HBA">Number of HBA</option>
+                                                <option value="Num_Amide_Bonds">Number of Amide Bonds</option>
+                                            </optgroup>
+                                            <optgroup label="Ring Types">
+                                                <option value="Num_Aromatic_Rings">Number of Aromatic Rings</option>
+                                                <option value="Num_Saturated_Rings">Number of Saturated Rings</option>
+                                                <option value="Num_Aliphatic_Rings">Number of Aliphatic Rings</option>
+                                            </optgroup>
                                         </select>
+
+
                                     </td>
                                     <td data-label="Condition">
                                         <select
--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -1,5 +1,3 @@
-// SearchPage.js
-
 import React, { useState } from 'react';
 import './styles.css';
 import Navbar from '../components/Navbar';
@@ -7,29 +5,102 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const SearchPage = () => {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchFields, setSearchFields] = useState({
+    "InChIKey": false,
+    "Compound InChI": false,
+    "Smiles": false,
+    "Molecular Formula": false,
+    "IUPAC Name": false,
+    "Compound Name": false,
+    "Origin type": false,
+    "Genus": false,
+    "Origin species": false,
+    "Peptaloid id": false,
+  });
 
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    return (
-        <div>
-            <div className="container">
-                <h2 className="heading">Welcome to the Alkaloid Database Search Page</h2>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                    className="search-bar"
-                />
-                <p className="paragraph">Enter your search query above to explore our database of alkaloids.</p>
-                {/* Add more content here */}
-            </div>
-            <Footer />
+  const handleFieldChange = (event) => {
+    setSearchFields({
+      ...searchFields,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const handleSearch = () => {
+    // Perform search using searchTerm and searchFields
+  };
+
+  const fields = Object.keys(searchFields);
+  const midIndex = Math.ceil(fields.length / 2);
+  const firstColumnFields = fields.slice(0, midIndex);
+  const secondColumnFields = fields.slice(midIndex);
+
+  return (
+    <div>
+      <div className="container">
+        <h2 className="heading">Welcome to the Alkaloid Database Search Page</h2>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="search-bar"
+        />
+          <p className="paragraph">
+            To speed up the search, please select a field to get the desired result. You can search by any of the following fields:
+          </p>
+        <div className="search-fields">
+          <div className="search-fields-column">
+            {firstColumnFields.map((field) => (
+              <div key={field} className="checkbox-wrapper-26">
+                <label htmlFor={`_checkbox-${field}`}>
+                  <input
+                    type="checkbox"
+                    id={`_checkbox-${field}`}
+                    name={field}
+                    checked={searchFields[field]}
+                    onChange={handleFieldChange}
+                  />
+                  <div className="tick_mark"></div>
+                  <span className={`search-field-label ${searchFields[field] ? 'selected' : ''}`}>
+                    {field}
+                  </span>
+                </label>
+              </div>
+            ))}
+          </div>
+          <div className="search-fields-column">
+            {secondColumnFields.map((field) => (
+              <div key={field} className="checkbox-wrapper-26">
+                <label htmlFor={`_checkbox-${field}`}>
+                  <input
+                    type="checkbox"
+                    id={`_checkbox-${field}`}
+                    name={field}
+                    checked={searchFields[field]}
+                    onChange={handleFieldChange}
+                  />
+                  <div className="tick_mark"></div>
+                  <span className={`search-field-label ${searchFields[field] ? 'selected' : ''}`}>
+                    {field}
+                  </span>
+                </label>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+        <button onClick={handleSearch} className="search-button">
+          Search
+        </button>
+        <p className="paragraph">Enter your search query above to explore our database of alkaloids.</p>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default SearchPage;
