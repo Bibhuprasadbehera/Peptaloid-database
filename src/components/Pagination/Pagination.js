17e8e5bbb11917b574ef84556f2a20ab036cf84c
Bibhuprasadbehera <53312940+Bibhuprasadbehera@users.noreply.github.com>
2024-08-04 23:23:20 +0530

    reverted pagination to old version

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import MoleculeCard from '../Molecules/MoleculeCard';
 import './pagination.css';
